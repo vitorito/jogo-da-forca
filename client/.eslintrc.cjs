6cc430c84deef64e51eec5fa88d8997272dcbab6
vitorito <victorolivbati@gmail.com>
2022-10-03 16:23:53 -0300

    Initial commit

---
+++ client/.eslintrc.cjs
@@ -0,0 +1,28 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "prettier"
+    ],
+    "overrides": [
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module",
+        "ecmaFeatures": {
+            "jsx": true
+        }
+    },
+    "plugins": [
+        "react"
+    ],
+    "ignorePatterns": ["dist"],
+    "rules": {
+        "react/react-in-jsx-scope": "off",
+    }
+};
