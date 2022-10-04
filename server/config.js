6905087d552a0750aaf85037ef6d3635eb69847f
vitorito <victorolivbati@gmail.com>
2022-10-04 07:21:46 -0300

    Create socket server

---
+++ server/config.js
@@ -0,0 +1,4 @@
+export default {
+  PORT: 3000,
+  CLIENT_URL: 'http://localhost:5173'
+};
--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,10 @@
-console.log('banana');
+import { Server } from 'socket.io';
+import cfg from '../config.js';
+
+const io = new Server(cfg.PORT, {
+  cors: [cfg.CLIENT_URL]
+});
+
+io.on('connection', socket => {
+  console.log(socket.id);
+});
