49cb60699707b5b31d1fcdc401ad2b6f464a07c8
vitorito <victorolivbati@gmail.com>
2022-10-04 07:20:44 -0300

    Add useNickname

--- client/src/hooks/useNickname.js
+++ client/src/hooks/useNickname.js
@@ -1,7 +1,36 @@
-import { useState } from 'react';
+import random from 'lodash/random';
+import { useEffect, useState } from 'react';
+import store from 'store2';
+
+function generateGuestNick() {
+  const base = 'Anonimo';
+  const minId = 100;
+  const maxId = 999;
+  const id = random(minId, maxId, false);
+  return base + id;
+}
+
+function loadNick() {
+  const alt = {
+    value: generateGuestNick(),
+    save: false,
+  };
+  return store.get('nick', alt);
+}
+
+function saveNick(nick) {
+  if (nick.save) {
+    store.set('nick', nick);
+  } else {
+    store.remove('nick');
+  }
+}
 
 function useNickname() {
-  const [nick, setNick] = useState('');
+  const [nick, setNick] = useState(loadNick());
+
+  useEffect(() => saveNick(nick), [nick]);
+
   return [nick, setNick];
 }
 
--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -5,20 +5,30 @@ import { HomeContainer } from './styled';
 function Home() {
   const [nick, setNick] = useNickname();
 
-  function handleInputChange({ target }) {
-    setNick(target.value);
+  function handleInputChange(key, value) {
+    setNick((prev) => ({ ...prev, [key]: value }));
   }
 
   return (
     <Container>
       <HomeContainer>
-        <label htmlFor="user">
+        <label htmlFor="nick">
           User
           <input
+            id="nick"
             type="text"
-            value={nick}
-            onChange={handleInputChange}
+            value={nick.value}
+            onChange={(e) => handleInputChange('value', e.target.value)}
             maxLength={30}
+          />
+        </label>
+        <label htmlFor="save">
+          Save
+          <input
+            id="save"
+            type="checkbox"
+            checked={nick.save}
+            onChange={(e) => handleInputChange('save', e.target.checked)}
           />
         </label>
       </HomeContainer>
