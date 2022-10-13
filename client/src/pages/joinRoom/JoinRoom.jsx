cd0d4087dbecd6ef4b4e16fd83961c47c3c0273e
vitorito <victorolivbati@gmail.com>
2022-10-13 20:31:54 -0300

    Update JoinRoom page

--- client/src/pages/joinRoom/JoinRoom.jsx
+++ client/src/pages/joinRoom/JoinRoom.jsx
@@ -1,5 +1,5 @@
 import { useContext, useState } from 'react';
-import PrevPageButton from '../../components/PrevPageButton';
+import { Link } from 'react-router-dom';
 import { NicknameContext } from '../../providers/NicknameProvider';
 
 const MAX_ROOM_ID = 9999;
@@ -7,37 +7,32 @@ const MAX_ROOM_ID = 9999;
 function JoinRoom() {
   const { nick } = useContext(NicknameContext);
   const [roomId, setRoomId] = useState('');
-  const [roomPassword, setRoomPassword] = useState('');
   const [isJoinRoomBtnDisabled, setIsJoinRoomBtnDisabled] = useState(true);
 
-  function updateJoinRoomBtn(id, password) {
-    const isValidForm = validateRoomId(id) && validateRoomPassword(password);
-    setIsJoinRoomBtnDisabled(!isValidForm);
+  function updateJoinRoomBtn(id) {
+    const isValidId = validateRoomId(id);
+    setIsJoinRoomBtnDisabled(!isValidId);
   }
 
   function handleRoomIdChange(e) {
     const newRoomId = e.target.value;
-    console.log(newRoomId);
+
     if (newRoomId.length > 4) return;
 
     setRoomId(() => newRoomId);
-    updateJoinRoomBtn(newRoomId, roomPassword);
-  }
-
-  function handleRoomPasswordChange(e) {
-    const newRoomPassword = e.target.value.trim();
-    setRoomPassword(() => newRoomPassword);
-    updateJoinRoomBtn(roomId, newRoomPassword);
+    updateJoinRoomBtn(newRoomId);
   }
 
   return (
-    <div className="main-container">
-      <h1 className="text-3xl text-center">
+    <div className="main-container sm:justify-center">
+      <h1 className="text-3xl text-center mb-16">
         <span>Junte-se a uma Sala</span>
       </h1>
-      <div className="sm:shadow-lg sm:shadow-yellow-600 rounded-md sm:py-6 sm:px-5 m-auto">
-        <div className="form-container m-auto text-lg">
-          <div className="text-2xl m-auto">{nick.value}</div>
+      <div className="sm:shadow-lg sm:shadow-yellow-600 rounded-md sm:py-6 sm:px-5 m-auto sm:m-0">
+        <div className="form-container text-lg">
+          <div className="text-2xl text-center font-semibold mb-6">
+            {nick.value}
+          </div>
           <form>
             <label htmlFor="roomId">
               <span>ID da Sala</span>
@@ -50,17 +45,6 @@ function JoinRoom() {
                 className="input mb-2"
               />
             </label>
-            <label htmlFor="roomPassword">
-              <span>Senha</span>
-              <input
-                type="password"
-                value={roomPassword}
-                onChange={handleRoomPasswordChange}
-                placeholder="4 Digits..."
-                maxLength={4}
-                className="input"
-              />
-            </label>
             <button
               disabled={isJoinRoomBtnDisabled}
               className="btn mt-4 mb-2 sm:mb-3"
@@ -69,7 +53,9 @@ function JoinRoom() {
               Entrar na Sala
             </button>
           </form>
-          <PrevPageButton />
+          <Link to="/" className="btn">
+            Voltar
+          </Link>
         </div>
       </div>
     </div>
@@ -80,11 +66,6 @@ function validateRoomId(id) {
   const hasCorrectLength = id.length === 4;
   const isWithinLimits = id >= 0 && id <= MAX_ROOM_ID;
   return hasCorrectLength && isWithinLimits;
-}
-
-function validateRoomPassword(roomPassword) {
-  const hasCorrectLength = roomPassword.length === 4;
-  return hasCorrectLength;
 }
 
 export default JoinRoom;
