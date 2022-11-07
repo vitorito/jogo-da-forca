d65a050fc607e7bfd9515a8b13a85794fd4796ae
vitorito <victorolivbati@gmail.com>
2022-11-07 10:35:27 -0300

    Sort the Ranking

--- client/src/pages/room/components/Ranking.jsx
+++ client/src/pages/room/components/Ranking.jsx
@@ -1,14 +1,18 @@
-import React, { useContext } from 'react';
+import React, { useContext, useMemo } from 'react';
 import ScrollableContainer from '../../../components/ScrollableContainer';
 import { MatchContext } from '../../../providers/MatchProvider';
 import RankingItem from './RankingItem';
 
 function Ranking() {
   const { room } = useContext(MatchContext);
+  const players = useMemo(
+    () => [...room.players].sort((p1, p2) => p2.points - p1.points),
+    [room]
+  );
   return (
     <ScrollableContainer className="sm-container bg-yellow-700">
       <ul className="flex flex-col gap-2 w-full h-full overflow-auto">
-        {room?.players.map((player, index) => (
+        {players.map((player, index) => (
           <RankingItem key={player.id} player={player} position={index + 1} />
         ))}
       </ul>
