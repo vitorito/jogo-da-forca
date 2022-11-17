51e11de7a125291aadf1798630fde6df04a0dd5d
vitorito <victorolivbati@gmail.com>
2022-11-17 15:19:06 -0300

    Add Room start test

--- server/src/models/Room.test.js
+++ server/src/models/Room.test.js
@@ -1,23 +1,24 @@
+/* eslint-disable no-undef */
 import gc from '../config/gameConstraints';
 import Player from './Player';
 import Room from './Room';
 
-/* eslint-disable no-undef */
+const player = new Player('socketId', '300001', 'nickname');
+const roomData = {
+  id: '3000',
+  owner: player,
+  password: '',
+  speed: 'fast',
+  themes: ['name', 'color'],
+  totalRounds: 10
+};
+
 describe('room creation', () => {
   test('should create a room', () => {
-    const owner = new Player('socketId', '300001', 'nickname');
-    const roomData = {
-      id: '3000',
-      owner,
-      password: '',
-      speed: 'fast',
-      themes: ['name', 'color'],
-      totalRounds: 10
-    };
     const room = new Room(roomData);
 
     expect(room.id).toBe(roomData.id);
-    expect(room.owner).toBe(owner);
+    expect(room.owner).toBe(player);
     expect(room.isPrivate).toBe(false);
     expect(room.password).toBe(roomData.password);
     expect(room.speed).toBe(roomData.speed);
@@ -25,7 +26,7 @@ describe('room creation', () => {
     expect(room.totalRounds).toBe(roomData.totalRounds);
     expect(room.currentRound).toBe(1);
     expect(room.players.size).toBe(1);
-    expect(room.players.has(owner.socketId)).toBe(true);
+    expect(room.players.has(player.socketId)).toBe(true);
     expect(room.playerInTurn).toBe('');
     expect(room.round.state).toBe(gc.ROOM_MATCH_STATES.waiting);
     expect(room.round.theme).toBe('');
@@ -33,5 +34,18 @@ describe('room creation', () => {
       players: [],
       themes: []
     });
+  });
+});
+
+
+describe('room start', () => {
+  test('should start the room', () => {
+    const room = new Room(roomData);
+    room.start();
+
+    expect(room.currentRound).toBe(1);
+    expect(room.playerInTurn).toBe(player.id);
+    expect(room.round.state).toBe(gc.ROOM_MATCH_STATES.running);
+    expect(roomData.themes).toContain(room.round.theme);
   });
 });
